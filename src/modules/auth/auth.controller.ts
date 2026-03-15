6a358531c383e65cba0211ea38aaf4d60f85949a
Mateus Santos <mateuspele2015@gmail.com>
2026-03-15 05:42:44 -0300

    feat(auth): add Google login endpoint to authentication controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { Public } from 'src/common/decorators/public';
+import { CreateAuthGoogleDto } from './dto/create-auth-google.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +13,10 @@ export class AuthController {
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
   }
+   @Public()
+    @Post('google')
+    loginWithGoogle(@Body() createAuthGoogleDto: CreateAuthGoogleDto) {
+        return this.authService.loginWithGoogle(createAuthGoogleDto);
+    }
 
 }
