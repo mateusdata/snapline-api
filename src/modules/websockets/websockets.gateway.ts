f524398203853d4728ca52b825b203f115bf413e
Mateus Santos <mateuspele2015@gmail.com>
2026-03-15 03:04:56 -0300

    feat: enhance websocket gateway with connection handling and logging

--- src/modules/websockets/websockets.gateway.ts
+++ src/modules/websockets/websockets.gateway.ts
@@ -1,16 +1,30 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
 import { WebsocketsService } from './websockets.service';
 import { CreateWebsocketDto } from './dto/create-websocket.dto';
 import { UpdateWebsocketDto } from './dto/update-websocket.dto';
 import { Public } from 'src/common/decorators/public';
-
-@WebSocketGateway({namespace: '/api/websockets'})
+import { Logger } from '@nestjs/common';
+import { Server } from 'socket.io';
+@WebSocketGateway({ namespace: '/api/websockets' })
 @Public()
 export class WebsocketsGateway {
-  constructor(private readonly websocketsService: WebsocketsService) {}
+  constructor(private readonly websocketsService: WebsocketsService) { }
+
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: any, ...args: any[]) {
+    Logger.log(`Ei client oreia seca você se conetou: ${client.id}`, 'WebsocketsGateway');
+  }
+
+  handleDisconnect(client: any) {
+    Logger.log(`Ei client oreia seca você se desconectou: ${client.id}`, 'WebsocketsGateway');
+  }
+
 
   @SubscribeMessage('createWebsocket')
   create(@MessageBody() createWebsocketDto: CreateWebsocketDto) {
+
     return this.websocketsService.create(createWebsocketDto);
   }
 
