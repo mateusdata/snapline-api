93999f80a49aa27adbd9058389fec45ec243af08
Mateus Santos <mateuspele2015@gmail.com>
2026-03-15 06:32:50 -0300

    fix(auth): extend JWT expiration time to 100 days

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { GameModule } from './modules/game/game.module';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: '100  d' },
     }),
     UsersModule,
     AuthModule,
