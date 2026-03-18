9c1327da3f9b5e393187f5f4e89de8c586f673bb
Mateus Santos <mateuspele2015@gmail.com>
2026-03-17 21:42:10 -0300

    feat(test): implement Test module with controller, service, DTOs, and tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { WebsocketsModule } from './modules/websockets/websockets.module';
 import { GameModule } from './modules/game/game.module';
 import { CallModule } from './modules/call/call.module';
+import { TestModule } from './modules/test/test.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { CallModule } from './modules/call/call.module';
     PrismaModule,
     GameModule,
     CallModule,
+    TestModule,
   ],
   controllers: [AppController],
   providers: [
---
+++ src/modules/test/test.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTestDto } from './dto/create-test.dto';
+import { UpdateTestDto } from './dto/update-test.dto';
+
+@Injectable()
+export class TestService {
+  create(createTestDto: CreateTestDto) {
+    return 'This action adds a new test';
+  }
+
+  findAll() {
+
+    const test = {
+      id: 1,
+      name: 'mateus@gmail.com',
+      description: 'user default'
+    }
+    return test
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} test`;
+  }
+
+  update(id: number, updateTestDto: UpdateTestDto) {
+    return `This action updates a #${id} test`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} test`;
+  }
+}
